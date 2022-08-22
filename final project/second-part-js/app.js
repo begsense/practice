43b06ab1f854c4d19c4ed729789d014047ea154b
begsense <begsense@gmail.com>
2022-08-22 11:21:46 +0400

    sawfa

--- final project/second-part-js/app.js
+++ final project/second-part-js/app.js
@@ -3,11 +3,11 @@ const box = document.querySelector('.box');
 let a = 2;
 damateba.addEventListener('click', function(){
     let label = document.createElement('label')
-    label.setAttribute('for', 'ganatleba1'+a)
+    label.setAttribute('for', 'ganatleba'+a)
     let input = document.createElement('input')
     input.setAttribute('type', 'text')
-    input.setAttribute('id', 'ganatleba1')
-    input.setAttribute('name', 'ganatleba1')
+    input.setAttribute('id', 'ganatleba'+a)
+    input.setAttribute('name', 'ganatleba'+a)
     label.innerText=('სათაური ' + a)
     box.appendChild(label)
     box.appendChild(input)
